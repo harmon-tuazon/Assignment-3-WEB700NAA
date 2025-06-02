ff4044308574a3f07e2fdc69d91c0cc14f8f7ddd
harmon-tuazon <harmontuazon@gmail.com>
2025-06-02 12:26:45 -0400

    initial commit

---
+++ modules/legoSets.js
@@ -0,0 +1,92 @@
+/********************************************************************************
+* WEB700 – Assignment 02
+*
+* I declare that this assignment is my own work in accordance with Seneca's
+* Academic Integrity Policy:
+*
+* https://www.senecapolytechnic.ca/about/policies/academic-integrity-policy.html
+*
+* Name: Harmon Tuazon Student ID: 165229220 Date: May 20,2025
+*
+********************************************************************************/
+
+class legoData {
+  constructor(sets = []) {
+    this.sets = sets;
+  }
+
+  initialize() {
+    return new Promise((resolve, reject) => {
+      try {
+        const setData = require("../data/setData");
+        const themeData = require("../data/themeData");
+
+        this.sets = [];
+
+        setData.forEach(set => {
+          let themeSet = themeData.find(theme => theme.id === set.theme_id);
+          let themeName;
+
+          if (themeSet) {
+            themeName = themeSet.name;
+          } else {
+            themeName = "Unknown";
+          }
+
+          this.sets.push({
+            ...set,
+            theme: themeName
+          });
+        });
+
+        resolve(); // Done initializing
+      } catch (error) {
+        reject("Failed to initialize data: " + error.message);
+      }
+    });
+  }
+
+  getAllSets() {
+    return new Promise((resolve, reject) => {
+      if (this.sets.length === 0) {
+        reject("No sets found. Did you run initialize()?");
+      } else {
+        resolve(this.sets);
+      }
+    });
+  }
+
+  getSetByNum(setNum) {
+    return new Promise((resolve, reject) => {
+      const foundSet = this.sets.find(set => set.set_num === setNum);
+
+      if (foundSet) {
+        resolve(foundSet);
+      } else {
+        reject("Set with set_num '" + setNum + "' not found.");
+      }
+    });
+  }
+
+  getSetsByTheme(theme) {
+    return new Promise((resolve, reject) => {
+      const chosenTheme = theme.toLowerCase();
+      const filteredSets = this.sets.filter(set => {
+        return set.theme.toLowerCase().includes(chosenTheme);
+      });
+
+      if (filteredSets.length > 0) {
+        resolve(filteredSets);
+      } else {
+        reject("No sets found with theme matching '" + theme + "'.");
+      }
+    });
+  }
+}
+
+
+
+
+
+
+module.exports = {legoData}
---
+++ server.js
@@ -0,0 +1,78 @@
+/********************************************************************************
+* WEB700 – Assignment 03
+*
+* I declare that this assignment is my own work in accordance with Seneca's
+* Academic Integrity Policy:
+*
+* https://www.senecapolytechnic.ca/about/policies/academic-integrity-policy.html
+*
+* Name: Harmon Tuazon Student ID: 165229220 Date: 06-02-2025
+*
+* Published URL: ___________________________________________________________
+*
+********************************************************************************/
+
+const LegoData = require("./modules/legoSets")
+const legoData = new LegoData()
+const express = require('express')
+const app = express()
+const HTTP_PORT = process.env.PORT || 8080;
+
+
+//http://localhost:8080/
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/home.html'))
+
+})
+
+//http://localhost:8080/about
+app.get("/about", (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/about.html'))
+})
+
+//http://localhost:8080/lego/sets
+app.get("/lego/sets", async (req, res) => {
+    try {
+        let sets;
+        if (req.query.theme) {
+            sets = await legoData.getSetsByTheme(req.query.theme);
+        } else {
+            sets = await legoData.getAllSets();
+        }
+        res.json(sets); // send response to client
+    } catch (error) {
+        res.status(404).send(`Error: ${error.message}`).sendFile((path.join(__dirname, '/views/about.html')));
+    }
+});
+
+//http://localhost:8080/lego/sets/:set_num
+app.get("/lego/sets/:set_num", async (req, res) => {
+    try {
+        let sets;
+        if (req.params.set_num) {
+            sets = await legoData.getSetByNum(req.params.set_num);
+        }
+
+        res.json(sets); // send response to client
+    } catch (error) {
+        res.status(404).sendFile(`Error: ${error.message}`).sendFile((path.join(__dirname, '/views/about.html')));
+    }
+});
+
+
+
+
+//makes an the application listen for requests
+async function listenChecker() {
+    try{
+        await legoData.initialize();
+        app.listen(HTTP_PORT, () => {
+            console.log('Server started....')
+        })
+    } 
+    
+    catch(error){
+    console.log(error.message)
+    }
+}
+listenChecker()
